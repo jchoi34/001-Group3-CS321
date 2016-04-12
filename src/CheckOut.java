fda95bd29f5ba4c84c4b786f97206b7ff6dceefe
jchoi34 <jchoi34@gmu.edu>
2016-04-12 19:16:52 -0400

    CheckOut.java edit 2

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -1,17 +1,19 @@
 
 public class CheckOut {
 
-	Customer c = new Customer();
-	boolean validation;
-	Reservation res;
-	Customer name;
+	protected Customer c = new Customer();
+	protected boolean validation;
+	protected Reservation res;
+	protected Customer name;
 	public void checkOut(Customer c)
 	{
+		if(validateUserInformation(c)){
 		c.STATUS_CHECKED_OUT = true;   // update checked out status
 		c.STATUS_CHECKED_IN = false;   // update checked in status
+		}
 	}
 	
-	boolean validateUserInformation(){
+	boolean validateUserInformation(Customer c){
 		try {
 			//Search for Customer and Reservation
 			name = Framework.getCustomerByName(c.name); // find customer by name
