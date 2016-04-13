3c0dd44776d505e86e390b55d1769627cd4cb005
Nick Bennett <Nick Bennett>
2016-04-13 16:35:04 -0400

    Fixing off by one error

--- src/BankingSystem.java
+++ src/BankingSystem.java
@@ -6,45 +6,44 @@ BankingSystem Psuedocode
 
 public class BankingSystem
 {
-	boolean validateCreditCard(String type, int number, String date)
+	boolean validateCreditCard(String ccType, String ccNumber, String ccExpiration)
 	{
 		//Card type check
 		boolean verified = true;
-		if (!(type.equalsIgnoreCase("Visa") || type.equalsIgnoreCase("MasterCard") || type.equalsIgnoreCase("American Express") || type.equalsIgnoreCase("Discover")))
+		if (!(ccType.equalsIgnoreCase("Visa") || ccType.equalsIgnoreCase("MasterCard") || ccType.equalsIgnoreCase("American Express") || ccType.equalsIgnoreCase("Discover")))
 			throw new RuntimeException("Card Verification Error: 1");
 
 		//Expiration date check
 		try
 		{
-			int month = Integer.parseInt(date.substring(0, date.indexOf('/'))); //Not needed, but just to be thorough
-			int year = Integer.parseInt(date.substring(date.indexOf('/')));
+			int month = Integer.parseInt(ccExpiration.substring(0, ccExpiration.indexOf('/'))); //Not needed, but just to be thorough
+			int year = Integer.parseInt(ccExpiration.substring(ccExpiration.indexOf('/')+1));
 			if(year < 2016)
 				throw new RuntimeException("Card Verification Error: 2");
 		}
 		catch (StringIndexOutOfBoundsException e){throw new RuntimeException("Card Verification Error: 2");}
 
 		//Card number check
-		int last = number % 10;
-		int reversed = 0;
+		int last = ccNumber.charAt(ccNumber.length()) - '0';
+		ccNumber = ccNumber.substring(0,ccNumber.length());
 
-		number /= 10;		//Reverse the remaining numbers
-		while(number != 0)
-		{
-			reversed *= 10;
-			reversed += number % 10;
-			number /= 10;
-		}
+		String reversed = "";	//Reverse the remaining numbers
+		for(int r = 0; r < ccNumber.length(); r++)
+			reversed = reversed + ccNumber.charAt(r);
 
-		String revString = String.valueOf(reversed);	//Convert reversed int to an array of digits
-		int[] digArray = new int[revString.length()];
-		for(int i = 0; i < revString.length(); i++)
-			digArray[i] = revString.charAt(i) - '0';
+		int[] digArray = new int[reversed.length()];	//Convert reversed number to an array of digits
+		for(int i = 0; i < reversed.length(); i++)
+			digArray[i] = reversed.charAt(i) - '0';
 
 		for(int j = 0; j <= 14; j+=2)	//Multiply every other digit by 2
 		{
-			digArray[j] *= 2;
-			if(digArray[j] > 9)
-				digArray[j] -= 9;
+			try
+			{
+				digArray[j] *= 2;
+				if(digArray[j] > 9)
+					digArray[j] -= 9;
+			}
+			catch (ArrayIndexOutOfBoundsException e){throw new RuntimeException("Card Verification Error: 2");}
 		}
 		
 		int sum = last;	//Get sum of digits
